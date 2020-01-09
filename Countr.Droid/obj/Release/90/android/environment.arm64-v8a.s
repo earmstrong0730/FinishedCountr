2b8bde73f8f804a12f62974789f9d27fdb26dc01
Elizabeth Armstrong <earmstrong0730@gmail.com>
2020-01-09 15:43:23 -0800

    Release actually works in Appcener this time.....Hopefully

--- Countr.Droid/obj/Release/90/android/environment.arm64-v8a.s
+++ Countr.Droid/obj/Release/90/android/environment.arm64-v8a.s
@@ -63,7 +63,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"883581ad-0063-4622-9f9f-9fb3bd7ae4d6"
+	.asciz	"7cb8c009-3705-4d9f-b2ff-40d35cde7034"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
